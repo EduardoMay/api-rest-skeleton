6b3751d755d2c9aed7da578f4e040f8d6c772401
EduardoMay <eduardo_may@outlook.com>
2021-04-09 13:55:07 -0500

    feat: integrar typescript y babel en el proyecto

--- routes/example.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const ExampleController = require("../controllers/example.controller");
-
-const router = new Router();
-
-router.get("", ExampleController.getExample);
-
-module.exports = router;
